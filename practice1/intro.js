a97047466a0efab530447278cf7da619436bad39
Maghan Das <dasmaghan23@gmail.com>
2025-09-29 22:28:10 +0000

    intro to js

---
+++ practice1/intro.js
@@ -0,0 +1,29 @@
+
+
+// let name = prompt("enter your name")
+// alert("welcome, " + name + " !")
+
+let age = 22;           // number
+let name = "John";      // string
+let isLoggedIn = true;  // boolean
+let notDefined;         // undefined
+let emptyValue = null;  // null
+
+let x = 5
+let y = 10
+
+// console.log("sum is " + (x+y))
+// console.log(x>y)
+
+
+// functions in js
+// function greet(name) {
+//     return "Hello " + name
+// }
+
+// console.log(greet("ALice"))
+
+// Arrow Functions 
+const greet = (name, das) => `Hello ${name} ${das}`;
+console.log(greet("ALice", "das"))
+
